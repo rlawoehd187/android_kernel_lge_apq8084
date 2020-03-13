3e22e512067f9d3d1e70cab5d83b59ac5cf01e0f
hiru <rlawoehd187@naver.com>
2020-03-13 16:15:24 +0900

    Revert "color eggs~"
    This reverts commit 9fb96a4d38372b1c3ebdc14b7ad6cb0fc0053138.

--- drivers/cpufreq/cpufreq_performance.c
+++ drivers/cpufreq/cpufreq_performance.c
@@ -37,7 +37,7 @@ static int cpufreq_governor_performance(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_performance = {
-	.name		= "武松",
+	.name		= "performance",
 	.governor	= cpufreq_governor_performance,
 	.owner		= THIS_MODULE,
 };
