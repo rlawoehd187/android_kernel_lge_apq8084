86baa7ce85ebefafaf57f2bd407bd36f649e1fe1
rlawoehd187 <rlawoehd187@naver.com>
2020-08-18 08:57:15 +0900

    drivers: adjust thermal limit

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -76,12 +76,12 @@ static struct msm_thermal_data msm_thermal_info;
 static struct msm_thermal_data_intelli msm_thermal_info_local = {
 	.sensor_id = 5,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 52,
+	.limit_temp_degC = 80,
 	.temp_hysteresis_degC = 5,
-	.limit_safe_temp_degC = 60,
-	.freq_step = 2,
+	.limit_safe_temp_degC = 90,
+	.freq_step = 5,
 	.freq_control_mask = 0xf,
-	.core_limit_temp_degC = 80,
+	.core_limit_temp_degC = 90,
 	.core_temp_hysteresis_degC = 5,
 	.core_control_mask = 0xc,
 };
